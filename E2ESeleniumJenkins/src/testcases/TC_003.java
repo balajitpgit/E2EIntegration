a534e00cec2108733cc60799ef01f5d0123c199d
BS112524 <BS112524@INCHCMLP03299.TechMahindra.com>
2020-08-17 13:21:20 +0530

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC_003.java
@@ -0,0 +1,19 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+
+	@Test
+	public void testcase1() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("http://www.facebook.com");
+		driver.findElement(By.id("email")).sendKeys("hello");
+		driver.findElement(By.id("pass")).sendKeys("hello1");
+		driver.quit();
+	}
+	
+}
